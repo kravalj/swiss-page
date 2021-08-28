7e96a2a3aaa9b73f386193f07b9c5da9c8defc13
kravalj <j.kraval.ml@gmail.com>
2021-08-28 18:18:37 +0200

    Project correction

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import './styles/MainPage.css';
 import Logo from '../Images/Logo.png';
-import { Link } from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
 
 
 const MainPage = () => {
 
-  return (
-    <div className="App">
-      <header className='App-header'>
+    return (
+        <div className="App">
+            <header className='App-header'>
 
-        <Link to="/WelcomePage">
-          <img src={Logo} className="App-logo" alt="logo" style={{ width: '1400px' }} />
-        </Link>
+                <Link to="/WelcomePage">
+                    <img src={Logo} className="App-logo" alt="logo" style={{width: '1400px'}}/>
+                </Link>
 
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default MainPage;
